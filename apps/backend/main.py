7791df86aca3bc9b344aaaefea459bd818c35308
Federico Guarin <federico.guarin@dataenthusiast.dev>
2025-10-25 23:33:17 -0500

    initialize backend

---
+++ apps/backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_headers=["*"],
+    allow_methods=["*"]
+)
+
+@app.get("/")
+async def root():
+    return {"message":"Welcome to fastAPI"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
