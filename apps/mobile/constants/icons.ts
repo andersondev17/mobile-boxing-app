fc5ef7a0a03ea8391fa67a60f931d66edbb9a9d8
Anderson Lopez <52222621+TheGodFatherpte@users.noreply.github.com>
2025-11-04 21:16:08 -0500

    revamp exercise details screen layout

--- apps/mobile/constants/icons.ts
+++ apps/mobile/constants/icons.ts
@@ -1,7 +1,12 @@
 import arrow from "@/assets/icons/arrow.png";
+import back from "@/assets/icons/back.png";
+import bell from "@/assets/icons/bell.png";
 import close from "@/assets/icons/close.png";
+import edit from "@/assets/icons/edit.png";
+import google from "@/assets/icons/google.png";
 import home from "@/assets/icons/home.png";
 import logo from "@/assets/icons/logo.png";
+import logout from "@/assets/icons/logout.png";
 import person from "@/assets/icons/person.png";
 import play from "@/assets/icons/play.png";
 import save from "@/assets/icons/save.png";
@@ -20,5 +25,9 @@ export const icons = {
   play,
   arrow,
   close,
-  share
+  share,
+  back,
+  edit,
+  logout,
+  google
 };
